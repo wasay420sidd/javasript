7b8db31fdf712e9bde35512d7ec7b9540945e352
wasay420sidd <102170023+wasay420sidd@users.noreply.github.com>
2022-04-19 17:50:04 +0500

    Add files via upload
    Marksheet

---
+++ Formula.js
@@ -0,0 +1,45 @@
+// var r = parseFloat(prompt("Enter RADIUS"));
+// var h = parseInt(prompt( "Enter HEIGHT")) ;
+// var AreaofCone = (3.14 * r ) * (r + ( h*h + r*r)) ;
+// document.write ("Result Is Area of Cone :" + AreaofCone )
+
+
+// var r = parseFloat(prompt("Enter RADIUS"));
+// var h = parseInt(prompt( "Enter HEIGHT")) ;
+// var AreaofArea = ( 2 * 3.14 * r * h ) + ( 2 * 3.14 * r * r ) ;
+// document.write ("Result Is Area of Area :" + AreaofArea )
+
+// var r = parseFloat(prompt("Enter RADIUS"));
+// var h = parseInt(prompt( "Enter HEIGHT")) ;
+// var AreaofVolmue = ( 3.14 * r * r * h ) ;
+// document.write ("Result Is Area of Volmue :" + AreaofVolmue )
+
+
+// var vf = parseFloat(prompt("Enter RADIUS"));
+// var vi = parseInt(prompt( "Enter HEIGHT")) ;
+// var a = parseInt(prompt( "Enter A")) ;
+// var Distance = ( (vf * vf ) - (vi * vi) ) / (2 * a) ;
+// document.write ("Result Is Distance :" + Distance )
+
+
+// var t = parseFloat(prompt("Enter RADIUS"));
+// var vi = parseInt(prompt( "Enter HEIGHT")) ;
+// var a = parseInt(prompt( "Enter A")) ;
+// var Distances = ( (vi * t ) + (( 1 * a * t * t ) / (2))) ;
+// document.write ("Result Is Distances :" + Distances )
+
+
+
+var bache = (prompt (" Bache tmhara nam kia hai ?")) ;
+var english = parseFloat( prompt (" Bache  english ke number batao ?")) ;
+var science = parseFloat( prompt (" Bache  science ke number batao ?")) ;
+var physics = parseFloat ( prompt(" Bache  physics ke number batao ?")) ;
+var chemistry = parseFloat( prompt (" Bache  chemistry ke number batao ?")) ;
+var TOTAL =  ( english + science + physics + chemistry)
+var per = ( TOTAL / 200) * (100)
+
+
+document.write ( bache + " your total Number :" + TOTAL +  " your Percentage :" + per + "% ")
+
+
+
